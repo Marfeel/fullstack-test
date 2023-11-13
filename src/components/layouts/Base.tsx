469fc41e0a7e48203300625fc10e637a0765e206
Dawid Zawadzki <dawidzki042@gmail.com>
2023-11-13 09:21:16 +0100

    lint(react): Lint using prettier

--- src/components/layouts/Base.tsx
+++ src/components/layouts/Base.tsx
@@ -1,21 +1,22 @@
-import React, { PropsWithChildren } from 'react';
-import Box from '@mui/material/Box';
-import { AppBar } from '../app-bar';
+import React, { PropsWithChildren } from "react";
+import Box from "@mui/material/Box";
+import { AppBar } from "../app-bar";
+import { Container } from "@mui/material";
 
 export const BaseLayout = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <AppBar title='Traffic' />
+    <Box sx={{ display: "flex" }}>
+      <AppBar title="Traffic" />
       <Box
-          component="main"
-          sx={{
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-            marginTop: '64px'
-          }}
-        >
-          { children }
+        component="main"
+        sx={{
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+          marginTop: "64px",
+        }}
+      >
+        <Container>{children}</Container>
       </Box>
     </Box>
   );
