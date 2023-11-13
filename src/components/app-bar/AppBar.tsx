095e5fb987e209f77c6f2fdb5141914e80635853
Dawid Zawadzki <dawidzki042@gmail.com>
2023-11-13 09:22:02 +0100

    feat(react): Create select on the topbar

--- src/components/app-bar/AppBar.tsx
+++ src/components/app-bar/AppBar.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import MuiAppBar from '@mui/material/AppBar';
-import { styled } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
+import React, { useState } from "react";
+import MuiAppBar from "@mui/material/AppBar";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const StyledAppBar = styled(MuiAppBar)`
   padding: 12px 30px;
@@ -13,6 +20,18 @@ export type AppBarProps = {
 };
 
 export const AppBar = ({ title }: AppBarProps) => {
+  const [selectedValue, setSelectedValue] = useState<number | string>("today");
+  const navigate = useNavigate();
+
+  const location = useLocation();
+  const handleSelectChange = (event: SelectChangeEvent<string | number>) => {
+    const newValue = event.target.value as string;
+    setSelectedValue(newValue);
+    // Update the URL query parameter
+    const searchParams = new URLSearchParams(location.search);
+    searchParams.set("timeframe", newValue);
+    navigate(`${location.pathname}?${searchParams.toString()}`);
+  };
   return (
     <StyledAppBar>
       <Typography
@@ -20,10 +39,27 @@ export const AppBar = ({ title }: AppBarProps) => {
         variant="h6"
         color="inherit"
         noWrap
-        sx={{ flexGrow: 1, alignSelf: 'center' }}
+        sx={{ flexGrow: 1, alignSelf: "center" }}
       >
-        { title }
+        {title}
       </Typography>
+      <FormControl variant="outlined" sx={{ minWidth: 120, marginLeft: 2 }}>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Age"
+          value={selectedValue}
+          onChange={handleSelectChange}
+          sx={{ color: "inherit" }}
+          displayEmpty
+          inputProps={{ "aria-label": "Without label" }}
+        >
+          <MenuItem value={"today"}>Today</MenuItem>
+          <MenuItem value={"yesterday"}>Yesterday</MenuItem>
+          <MenuItem value={"last week"}>Last 7 days</MenuItem>
+          <MenuItem value={"this month"}>This month</MenuItem>
+        </Select>
+      </FormControl>
     </StyledAppBar>
   );
 };
