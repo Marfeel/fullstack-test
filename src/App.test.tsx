8c44a71a1ce46ca606a6f15c540ba23c29e68795
Nayah Pascual <nayahpascual@gmail.com>
2024-01-30 17:47:14 +0100

    feat(frontend): e2e testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders app properly', () => {
+test("renders app properly", () => {
   render(<App />);
 
   expect(true).toBeTruthy();
+});
+
+test("end to end test", async () => {
+  render(<App />);
+
+  // wait to load all first 10 articles
+  await waitFor(() => {
+    const articles = screen.getAllByRole("article");
+    expect(articles.length).toBe(10);
+  });
+  // wait to load the main chart
+  await waitFor(() => {
+    const mainChart = screen.getByRole("presentation");
+    expect(mainChart).toBeInTheDocument();
+  });
+
+  // click on the third article
+  const articles = screen.getAllByRole("article")[2];
+  fireEvent.click(articles);
+
+  // wait to load the single article page
+  await waitFor(() => {
+    const singleArticle = screen.getByRole("single-article");
+    expect(singleArticle).toBeInTheDocument();
+  });
 });
